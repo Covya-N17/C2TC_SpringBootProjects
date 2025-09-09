f36ff542dbe61f1e1c6585eefaef567a1e32c56e
Covya-N17 <covyanadarajan2004@gmail.com>
2025-09-09 14:58:07 +0530

    commit

---
+++ springbootbasic-1/src/main/java/com/IOC/DI/Springbootbasic1Application.java
@@ -0,0 +1,13 @@
+package com.IOC.DI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootbasic1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootbasic1Application.class, args);
+	}
+
+}
